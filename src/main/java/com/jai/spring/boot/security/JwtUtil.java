1c7c6d568658054e0035110f9f599fc7323248af
jai03sah <jaiprakashsah57@gmail.com>
2026-03-09 19:57:38 +0530

    fourth change

---
+++ src/main/java/com/jai/spring/boot/security/JwtUtil.java
@@ -0,0 +1,12 @@
+package com.jai.spring.boot.security;
+
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+public class JwtUtil {
+    private String secret = "mysecretkey";
+
+    public String generateToken(String email){
+        
+    }
+}
